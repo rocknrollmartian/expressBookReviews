7d4d71b517e462e47d339529713922e122aabf83
Meredith <mbellomy81@gmail.com>
2023-06-22 18:39:43 -0400

    changes added to general folder

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,10 +26,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
   public_users.get('/',function (req, res) {
     //Write your code here
-   public_users.get('/review/:isbn',function (req, res) {
-      const isbn = req.params.isbn;
-      res.send(books[isbn]["reviews"])
-    });  
+    res.send(JSON.stringify(books,null,4));
+    return res.status(300).json({message: "Yet to be implemented"});  
 });
 
 // Get book details based on ISBN
